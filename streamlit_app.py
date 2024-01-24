9aa24ade75ce40f4b21c5563117c3e0d5c38fdcd
Kanwalpreet Kaur <88227023+kanwal-kingra@users.noreply.github.com>
2024-01-24 22:59:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
